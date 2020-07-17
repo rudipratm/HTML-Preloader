2f9acd2ab2ca6a377bfe4c12a21c61d05d76073b
rudi <rudi.pratm@gmail.com>
2020-07-17 15:32:17 +0800

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,140 @@
+<html>
+<head>
+    <title>PaperMC – The High Performance Fork</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://papermc.io/favicons/apple-touch-icon.png?v=BG70oBaPzN" />
+    <link rel="icon" type="image/png" sizes="32x32" href="https://papermc.io/favicons/favicon-32x32.png?v=BG70oBaPzN" />
+    <link rel="icon" type="image/png" sizes="16x16" href="https://papermc.io/favicons/favicon-16x16.png?v=BG70oBaPzN" />
+    <link rel="manifest" href="https://papermc.io/favicons/site.webmanifest?v=BG70oBaPzN" />
+    <link rel="mask-icon" href="https://papermc.io/favicons/safari-pinned-tab.svg?v=BG70oBaPzN" color="#5bbad5" />
+    <link rel="shortcut icon" href="https://papermc.io/favicons/favicon.ico?v=BG70oBaPzN" />
+    <meta name="msapplication-TileColor" content="#1886cd" />
+    <meta name="msapplication-config" content="https://papermc.io/favicons/browserconfig.xml?v=BG70oBaPzN" />
+    <meta name="theme-color" content="#1886cd" />
+    <link rel="stylesheet" type="text/css" href="style.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link type="text/css" rel="stylesheet" href="https://papermc.io/css/materialize.min.css?v=a5600e4a74937f46" media="screen, projection" />
+    <link type="text/css" rel="stylesheet" href="https://papermc.io/css/styles.css?v=74839f572e6b36fd" media="screen, projection" />
+    <link type="text/css" rel="stylesheet" href="https://papermc.io/css/index.css?v=b82c832eb8b09841" media="screen, projection" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="loader">
+        <div class="preloader loading">
+            <span class="slice"></span>
+            <span class="slice"></span>
+            <span class="slice"></span>
+            <span class="slice"></span>
+            <span class="slice"></span>
+            <span class="slice"></span>
+        </div>
+    </div>
+    <div class="navbar-fixed">
+        <nav class="grey darken-4">
+            <div class="nav-wrapper">
+                <div class="brand-logo">
+                    <a href="/"><img src="https://papermc.io/images/logo-marker.svg?v=2ca03d456bd669f9" /></a>
+                </div>
+                <a data-target="sidenav" class="hide-on-large-only white-text button-collapse sidenav-button sidenav-trigger clickable"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li>
+                        <a class="white-text btn btn-flat waves-effect" href="community"><i class="material-icons left">people</i>COMMUNITY</a>
+                    </li>
+                    <li>
+                        <a class="white-text btn btn-flat waves-effect" href="https://github.com/PaperMC"><i class="material-icons left">code</i>GITHUB</a>
+                    </li>
+                    <li>
+                        <a class="white-text btn btn-flat waves-effect" href="https://paper.readthedocs.io/"><i class="material-icons left">help_outline</i>DOCUMENTATION</a>
+                    </li>
+                    <li>
+                        <a class="downloads-button white grey-text text-darken-4 btn nav-btn waves-effect" href="downloads"><i class="material-icons left">cloud_download</i>Downloads</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <ul id="sidenav" class="sidenav">
+        <a href="/" class="grey-text text-darken-4 brand-logo"><img src="https://papermc.io/images/logo.svg?v=000c904bdf4fab46" /></a>
+        <li>
+            <a class="grey-text text-darken-4 waves-effect" href="/"><i class="material-icons left"></i>HOME</a>
+        </li>
+        <li>
+            <a class="grey-text text-darken-4 waves-effect" href="/community"><i class="material-icons left"></i>COMMUNITY</a>
+        </li>
+        <li>
+            <a class="grey-text text-darken-4 waves-effect" href="https://github.com/PaperMC/Paper"><i class="material-icons left github"></i>GITHUB</a>
+        </li>
+        <li>
+            <a class="grey-text text-darken-4 waves-effect" href="https://paper.readthedocs.io/"><i class="material-icons left"></i>DOCUMENTATION</a>
+        </li>
+        <li><a class="btn light-blue darken-2 waves-effect waves-light" href="downloads">Downloads</a></li>
+    </ul>
+    <main>
+        <div class="jumbotron white-text">
+            <h1>Turn your server up to 11.</h1>
+            <p>Paper is the next generation of Minecraft server, compatible with Spigot plugins and offering uncompromising performance.</p>
+        </div>
+        <section id="section-why" class="container">
+            <h3 class="center-align">Why use Paper?</h3>
+            <p class="center-align">Whether you're an administrator looking to boost your server or a developer looking for more functionality, Paper is ready for you.</p>
+            <div class="row">
+                <div class="col s12 l3"><i class="material-icons benefit-icon">flight_takeoff</i></div>
+                <div class="col s12 l9">
+                    <h4>It's stupid fast.</h4>
+                    <p>
+                        Paper contains numerous improvements and optimizations resulting in a significant improvement in performance. Paper also includes the next version of Timings, enabling you to quickly find out what's slowing down
+                        your server.
+                    </p>
+                    <a href="https://paper.readthedocs.io/" class="waves-effect waves-light btn light-blue darken-2"><i class="material-icons left">archive</i>Learn about Performance</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 l3"><i class="material-icons benefit-icon">people</i></div>
+                <div class="col s12 l9">
+                    <h4>An active and growing community.</h4>
+                    <p>
+                        Paper has an active and growing community of server administrators and developers. Problem? Come talk with us on Discord and get real time support. Want to contribute? Submit a pull request and get it reviewed
+                        this century.
+                    </p>
+                    <a href="community" class="waves-effect waves-light btn light-blue darken-2"><i class="material-icons left">archive</i>Join our Community</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 l3"><i class="material-icons benefit-icon">code</i></div>
+                <div class="col s12 l9">
+                    <h4>An expanded API.</h4>
+                    <p>
+                        Paper extends and improves the Bukkit and Spigot APIs so that you and your developers have more features and functionality at your fingertips. All the meanwhile, Paper retains compatibility with plugins written
+                        for Spigot and Bukkit.
+                    </p>
+                    <a href="https://papermc.io/javadocs/" class="waves-effect waves-light btn light-blue darken-2"><i class="material-icons left">archive</i>Learn about our API</a>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="page-footer grey darken-4">
+        <div class="footer-copyright">
+            <div class="container">
+                <span class="copyright">© 2020 The PaperMC Team</span>
+            </div>
+        </div>
+    </footer>
+    <script type="text/javascript" src="https://papermc.io/js/materialize.min.js?v=53f7070cc4c81c27"></script>
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+    <script type="text/javascript" src="https://papermc.io/js/script.js?v=be919b6e19fc22a6"></script>
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+    <script type="text/javascript">
+    	//Ini akan menghapus class loader (preloader halaman) ketika halaman berhasil dimuat
+        $(window).load(function(){
+            $('.loader').fadeOut('slow', function() {
+                $(this).remove();
+            });
+        });
+    </script>
+</body>
+</html>
+<!-- Kenapa saya menggunakan halaman Papermc sebagai uji coba? Karena menurut saya Papermc sangat cocok untuk dijadikan percobaan loading karena tidak terlalu banyak aset yang perlu dimuat. Tekan CTRL + L SHIFT + R untuk clear cache & refresh halaman untuk uji coba preloader.-->
